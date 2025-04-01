6ca439aee81fd760b00f8cb21d93c0bd5e24c1a5
Your Name <email@example.com>
2025-04-02 05:04:16 +1100

    Trying pandas method

--- src/pink_morsel_sales.py
+++ src/pink_morsel_sales.py
@@ -1,4 +1,5 @@
 import csv
+from operator import index
 import os
 import glob
 import pandas as pd
@@ -8,28 +9,49 @@ csv_dir = '../data'
 output_dir = '../output'
 output_file = os.path.join(output_dir, 'pink_morsel_sales.csv')
 
-# Making an output directory
+# Make output directory
 os.makedirs(output_dir, exist_ok=True)
 
-# Using glob to grab all csv files
+# Use glob to grab all csv files
 csv_files = glob.glob(os.path.join(csv_dir, '*.csv'))
 
-with open(output_file, mode='w') as output:
-    fieldnames = ['sales', 'date', 'region']
-    writer = csv.DictWriter(output, fieldnames=fieldnames)
-    writer.writeheader()
-    
+
+def standard():
+    with open(output_file, mode='w') as output:
+        fieldnames = ['sales', 'date', 'region']
+        writer = csv.DictWriter(output, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for csv_file in csv_files:
+            with open(csv_file) as open_file:
+                csv_reader = csv.reader(open_file, delimiter=',')
+                next(csv_reader)
+
+                # 0-name, 1-price, 2-quantity, 3-date, 4-region
+                for row in csv_reader:
+                    if row[0] == 'pink morsel':
+                        price = float(sub(r'[^\d.]', '', row[1]))
+                        sales = price * float(row[2])
+                        writer.writerow({'sales': sales, 'date': row[3], 'region': row[4]})
+
+
+def using_pandas():
+    dfs = []
     for csv_file in csv_files:
-        with open(csv_file) as open_file:
-            csv_reader = csv.reader(open_file, delimiter=',')
-            next(csv_reader)
+        df = pd.read_csv(csv_file)
+        
+        pink_morsel = df[df['product'] == 'pink morsel'].copy()
+        pink_morsel['price'] = pink_morsel['price'].str.replace(r'[^\d.]', '', regex=True).astype(float)
+        pink_morsel['quantity'] = pink_morsel['quantity'].astype(float)
+        pink_morsel['sales'] = pink_morsel['price'] * pink_morsel['quantity']
 
-            # 0-name, 1-price, 2-quantity, 3-date, 4-region
-            for row in csv_reader:
-                if row[0] == 'pink morsel':
-                    price = float(sub(r'[^\d.]', '', row[1]))
-                    sales = price * float(row[2])
-                    sales = "${:,.2f}".format(sales)
+        result = pink_morsel[['sales', 'date', 'region']]
+        dfs.append(result)
 
-                    writer.writerow({'sales': sales, 'date': row[3], 'region': row[4]})
+    df = pd.concat(dfs)
+    # df = df.sort_values(by='date', ascending=True)
+    df.to_csv(output_file, index=False)
+
     
+if __name__ == "__main__":
+    standard()
